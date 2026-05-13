ee30f7bd1cb08a807f37e2d3e04a10adf8135848
Pritam Raghuwanshi <rpreetam550@gmail.com>
2026-05-13 22:38:06 +0530

    updated

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -26,11 +26,12 @@ const createToken = (user) => {
 }
 
 const setCookie = (res, token) => {
- res.cookie("token", token, {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
-})
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000
+  })
 }
 
 
